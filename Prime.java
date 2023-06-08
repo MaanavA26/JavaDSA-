001fd3ec94b593625879640600361b06613ae7b4
MaanavA26 <123941207+MaanavA26@users.noreply.github.com>
2023-06-08 16:04:12 +0530

    Update Prime.java
    Find whether a given number is Prime or not using Recursion

--- Prime.java
+++ Prime.java
@@ -1,10 +1,5 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+/* Find whether a given number is Prime or not using Recursion
+*/
 
 public class Prime
 {
